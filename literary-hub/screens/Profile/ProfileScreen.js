ce9dfd9f43cff6ae156a0671bbecfbd1fd252697
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-10-18 16:56:56 -0400

    basic profile info

--- literary-hub/screens/Profile/ProfileScreen.js
+++ literary-hub/screens/Profile/ProfileScreen.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
 const ProfileScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ProfileScreen heheh</Text>
+      <Image 
+        source={{ uri: 'https://davidbruceblog.files.wordpress.com/2014/05/img_9760.jpg' }} 
+        style={{width: 100, height: 100, borderRadius: 100/ 2}} 
+      />
+      <Text style={styles.name}>Ava Robinson</Text>
+      <Text style={styles.username}>@dietcokelover89</Text>
+      
+      <View style={styles.metrics}>
+        <View style={styles.metric}>
+          <Text style={styles.metricNumber}>26</Text>
+          <Text style={styles.metricName}>Collections</Text>
+        </View>
+
+        <View style={styles.metric}>
+          <Text style={styles.metricNumber}>244</Text>
+          <Text style={styles.metricName}>Followers</Text>
+        </View>
+
+        <View style={styles.metric}>
+          <Text style={styles.metricNumber}>57</Text>
+          <Text style={styles.metricName}>Following</Text>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
@@ -16,5 +38,32 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
+      paddingTop: 50,
     },
+
+    name: {
+      fontSize: 20
+    },
+
+    username: {
+      fontSize: 15
+    },
+
+    metrics: {
+      flex: 1,
+      flexDirection: 'row',
+      columnGap: 20,
+    },
+
+    metric: {
+      alignItems: 'center',
+    },
+
+    metricNumber: {
+      fontSize: 20,
+    },
+
+    metricName: {
+
+    }
   });
