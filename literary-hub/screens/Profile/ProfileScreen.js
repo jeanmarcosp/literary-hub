afb22272423c959e6662b76b6c673ada661dfdb3
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-10-22 22:27:30 -0400

    segmented control conditional rendering and collections view

--- literary-hub/screens/Profile/ProfileScreen.js
+++ literary-hub/screens/Profile/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native'
 import { React, useState }from 'react'
 import { Ionicons } from "@expo/vector-icons";
 
@@ -8,74 +8,153 @@ const ProfileScreen = () => {
 
   const [segmentedControlView, setSegmentedControlView] = useState('Collections')
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image 
-        source={{ uri: 'https://davidbruceblog.files.wordpress.com/2014/05/img_9760.jpg' }} 
-        style={styles.profilePic}
+  const username = 'dietcokelover89'
+
+  const collectionData = [
+    {
+      id: '1',
+      title: 'Zen zone',
+      poemNumber: 26,
+      author: 'dietcokelover89',
+      image: 'https://media.istockphoto.com/id/1391404962/photo/pyramid-stones-balance-on-old-mossy-fallen-tree.webp?b=1&s=170667a&w=0&k=20&c=k-eIZWcTHtTvcBCUlckZ4Q6wo5iPJKphu-iPfs_xgjA='
+    },
+    {
+      id: '2',
+      title: 'Haiku-palooza',
+      poemNumber: 255,
+      author: 'someone',
+      image: 'https://davidbruceblog.files.wordpress.com/2014/05/img_9760.jpg'
+    },
+  ]
+
+  const CollectionsView = () => {
+    return (
+      <FlatList
+        data={collectionData}
+        renderItem={({item}) => 
+          <Collection
+            title={item.title}
+            poemNumber={item.poemNumber}
+            author={item.author}
+            image={item.image}
+          />
+        }
+        keyExtractor={item => item.id}
+        style={styles.collections}
       />
-      
-      <View style={styles.names}>
-        <Text style={styles.name}>Ava Robinson</Text>
-        <Text style={styles.username}>@dietcokelover89</Text>
-      </View>
+    )
+  }
 
-      <View style={styles.metrics}>
-        <View style={styles.metric}>
-          <Text style={styles.metricNumber}>26</Text>
-          <Text style={styles.metricName}>Collections</Text>
-        </View>
-
-        <View style={styles.metric}>
-          <Text style={styles.metricNumber}>244</Text>
-          <Text style={styles.metricName}>Followers</Text>
-        </View>
-
-        <View style={styles.metric}>
-          <Text style={styles.metricNumber}>57</Text>
-          <Text style={styles.metricName}>Following</Text>
-        </View>
-      </View>
-
+  const Collection = ({title, poemNumber, author, image}) => {
+    return (
       <TouchableOpacity>
-        <View style={styles.followButton}>
-          <Ionicons name="person-add-outline" size={17} color="white" />
-          <Text style={styles.followText}>Follow</Text>
+        <View style={styles.collection}>
+          <View style={styles.collectionMain}>
+            <Image
+              source={{ uri: `${image}` }}
+              style={styles.collectionPic}
+            />
+            <View style={styles.collectionText}>
+              <Text style={styles.collectionName}>{title}</Text>
+              <Text style={styles.collectionStat}>{poemNumber} poems</Text>
+            </View>
+          </View>
+
+          {author === username &&
+            <View style={styles.ownershipTag}>
+              <Text style={styles.ownershipText}>Self</Text>
+            </View>
+          }
         </View>
       </TouchableOpacity>
+    )
+  }
 
-      <View style={styles.segmentedControl}>
-        <TouchableOpacity onPress={() => setSegmentedControlView('Collections')}>
-          <View style={styles.segmentedControlSelected}>
-            <Text style={styles.segmentedControlSelectedText}>Collections</Text>
-          </View>
-        </TouchableOpacity>
-        
-        <TouchableOpacity onPress={() => setSegmentedControlView('Liked poems')}>
-          <View style={styles.segmentedControlUnselected}>
-            <Text style={styles.segmentedControlUnselectedText}>Liked poems</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => setSegmentedControlView('Saved quotes')}>
-          <View style={styles.segmentedControlUnselected}>
-            <Text style={styles.segmentedControlUnselectedText}>Saved quotes</Text>
-          </View>
-        </TouchableOpacity>
+  const LikedPoemsView = () => {
+    return (
+      <View>
+        <Text>Liked Poems</Text>
       </View>
+    )
+  }
 
-      <View style={styles.collection}>
-        <Image
-          source={{ uri: 'https://davidbruceblog.files.wordpress.com/2014/05/img_9760.jpg' }}
-          style={styles.collectionPic}
+  const SavedQuotesView = () => {
+    return (
+      <View>
+        <Text>Saved Quotes</Text>
+      </View>
+    )
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centerAligned}>
+        <Image 
+          source={{ uri: 'https://davidbruceblog.files.wordpress.com/2014/05/img_9760.jpg' }} 
+          style={styles.profilePic}
         />
-        <View style={styles.collectionText}>
-          <Text style={styles.collectionName}>Zen zone</Text>
-          <Text style={styles.collectionStat}>26 poems</Text>
-        </View>
         
+        <View style={styles.names}>
+          <Text style={styles.name}>Ava Robinson</Text>
+          <Text style={styles.username}>@dietcokelover89</Text>
+        </View>
+
+        <View style={styles.metrics}>
+          <View style={styles.metric}>
+            <Text style={styles.metricNumber}>26</Text>
+            <Text style={styles.metricName}>Collections</Text>
+          </View>
+
+          <View style={styles.metric}>
+            <Text style={styles.metricNumber}>244</Text>
+            <Text style={styles.metricName}>Followers</Text>
+          </View>
+
+          <View style={styles.metric}>
+            <Text style={styles.metricNumber}>57</Text>
+            <Text style={styles.metricName}>Following</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity>
+          <View style={styles.followButton}>
+            <Ionicons name="person-add-outline" size={17} color="white" />
+            <Text style={styles.followText}>Follow</Text>
+          </View>
+        </TouchableOpacity>
+
+        <View style={styles.segmentedControl}>
+          <TouchableOpacity onPress={() => setSegmentedControlView('Collections')}>
+            <View style={(segmentedControlView === 'Collections' ? styles.segmentedControlSelected : styles.segmentedControlUnselected)}>
+              <Text style={(segmentedControlView === 'Collections' ? styles.segmentedControlSelectedText : styles.segmentedControlUnselectedText)}>Collections</Text>
+            </View>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={() => setSegmentedControlView('Liked poems')}>
+            <View style={(segmentedControlView === 'Liked poems' ? styles.segmentedControlSelected : styles.segmentedControlUnselected)}>
+              <Text style={(segmentedControlView === 'Liked poems' ? styles.segmentedControlSelectedText : styles.segmentedControlUnselectedText)}>Liked poems</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setSegmentedControlView('Saved quotes')}>
+            <View style={(segmentedControlView === 'Saved quotes' ? styles.segmentedControlSelected : styles.segmentedControlUnselected)}>
+              <Text style={(segmentedControlView === 'Saved quotes' ? styles.segmentedControlSelectedText : styles.segmentedControlUnselectedText)}>Saved quotes</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
 
+      <View style={styles.leftAligned}>
+        {segmentedControlView === 'Collections' &&
+          <CollectionsView />
+        }
+        {segmentedControlView === 'Liked poems' &&
+          <LikedPoemsView />
+        }
+        {segmentedControlView === 'Saved quotes' &&
+          <SavedQuotesView />
+        }
+      </View>
     </SafeAreaView>
   )
 }
@@ -86,6 +165,9 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+    },
+
+    centerAligned: {
       alignItems: 'center',
     },
 
@@ -179,7 +261,22 @@ const styles = StyleSheet.create({
       fontSize: 15,
     },
 
+    leftAligned: {
+      paddingHorizontal: 17,
+    },
+
+    collections: {
+      marginTop: 20,
+    },
+
     collection: {
+      marginBottom: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    collectionMain: {
       flexDirection: 'row',
       alignItems: 'center',
       columnGap: 20,
@@ -202,4 +299,17 @@ const styles = StyleSheet.create({
     collectionStat: {
       fontSize: 15,
     },
+
+    ownershipTag: {
+      borderWidth: 1,
+      borderRadius: 100,
+      height: 25,
+      width: 60,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+
+    ownershipText: {
+      fontSize: 15,
+    }
   });
