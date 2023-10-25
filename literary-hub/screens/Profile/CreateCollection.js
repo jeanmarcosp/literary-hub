ed90405cc2fb0a8228ee73a8943267bcde112024
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-10-25 16:23:25 -0400

    added create collection CTA in profile and navigation to create collection screen

---
+++ literary-hub/screens/Profile/CreateCollection.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native'
+import { React, useState }from 'react'
+import { Ionicons } from "@expo/vector-icons";
+
+const CreateCollection = () => {
+    return (
+        <View>
+            <Text>Creating a collection screen</Text>
+        </View>
+    )
+}
+
+export default CreateCollection;
