6c237cf90e8a37e5384a87b294fef77a293faeb4
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-11-12 22:35:26 -0500

    list of poems in collection

--- literary-hub/screens/Explore/Collection.js
+++ literary-hub/screens/Explore/Collection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, FlatList } from 'react-native';
 import Like from '../../components/Like';
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
@@ -7,6 +7,52 @@ import { useNavigation } from '@react-navigation/native';
 const CollectionScreen = () => {
 
   const navigation = useNavigation();
+
+  const Poem = ({ author, title }) => {
+    return (
+      <View style={styles.poem}>
+        <View style={styles.poemInfo}>
+          <Text style={styles.poemName}>{title}</Text>
+          <Text style={styles.poemAuthor}>{author}</Text>
+        </View>
+        <Like />
+      </View>
+    )
+  }
+
+  const poemData = [
+    {
+      id: "1",
+      title: 'October',
+      author: 'Robert Frost',
+      liked: false
+    },
+    {
+      id: "2",
+      title: 'Beyond the Red River',
+      author: 'Thomas McGrath',
+      liked: false
+    },
+    {
+      id: "3",
+      title: 'For the Chipmunk in My Yard',
+      author: 'Robert Gibb',
+      liked: false
+    },
+  ];
+
+  const PoemList = () => {
+    return (
+      <FlatList
+        data={poemData}
+        renderItem={({ item }) => (
+          <Poem title={item.title} author={item.author} />
+        )}
+        keyExtractor={(item) => item.id}
+        style={styles.poemList}
+      />
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -20,6 +66,7 @@ const CollectionScreen = () => {
             <Ionicons name="chevron-back" size={23} color="#fff" />
           </View>
         </TouchableOpacity>
+
         <View style={styles.collectionInfo}>
           <Text style={styles.collectionName}>fall</Text>
           <Text style={styles.collectionAuthor}>@catlady123</Text>
@@ -29,7 +76,15 @@ const CollectionScreen = () => {
             <Text style={styles.collectionLikeNumber}>2.5k</Text>
           </View>
         </View>
+
+        <TouchableOpacity style={styles.readButtonWrapper}>
+          <View style={styles.readButton}>
+            <Text style={styles.readButtonText}>Read</Text>
+          </View>
+        </TouchableOpacity>
       </ImageBackground>
+
+      <PoemList />
     </View>
   );
 };
@@ -91,10 +146,55 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
 
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  readButtonWrapper: {
+    position: 'absolute',
+    right: 30,
+    bottom: -17,
   },
+
+  readButton: {
+    alignSelf: 'baseline',
+    backgroundColor: '#644980',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 100,
+  },
+
+  readButtonText: {
+    color: '#fff',
+    fontFamily: 'HammersmithOne',
+    fontSize: 15,
+  },
+
+  poemList: {
+    marginTop: 20,
+  },
+
+  poem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    paddingHorizontal: 40,
+  },
+
+  poemInfo: {
+
+  },
+
+  poemName: {
+    fontFamily: 'HammersmithOne',
+    fontSize: 20,
+    color: '#373F41'
+  },
+
+  poemAuthor: {
+    fontFamily: 'Sarabun-Regular',
+    fontSize: 15,
+    color: '#6C7476'
+  }
 });
 
 export default CollectionScreen;
