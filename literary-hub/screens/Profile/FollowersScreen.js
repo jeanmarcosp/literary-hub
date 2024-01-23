476d61094829ea603aeb41281488601a052b4af4
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2024-01-22 23:02:59 -0500

    created flatlist for followers screen

--- literary-hub/screens/Profile/FollowersScreen.js
+++ literary-hub/screens/Profile/FollowersScreen.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect, useCallback, useContext } from "react";
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Dimensions, FlatList} from "react-native";
-import OnboardingMockup from './../../assets/onboarding-images/onboarding-mockup.svg'
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import Follower from './../../components/Follower.js'
 
 const FollowersScreen = () => {
     const [segmentedControlView, setSegmentedControlView] = useState("Collections");
     const navigation = useNavigation();
+
+    const followers = [
+        {
+            id: '1'
+        },
+        {
+            id: '2'
+        },
+        {
+            id: '3'
+        },
+    ]
 
     return (
         <SafeAreaView>
@@ -20,21 +32,14 @@ const FollowersScreen = () => {
                     <Ionicons name="chevron-back-outline" size={22} color="#fff" />
                 </View>
 
-                <View style={styles.profile}>
-                    <View style={styles.profileInfo}>
-                        <View style={styles.profilePic}></View>
-                        <View style={styles.text}>
-                            <Text style={styles.username}>johndoe</Text>
-                            <Text style={styles.name}>John Doe</Text>
-                        </View>
-                    </View>
-
-                    <TouchableOpacity>
-                        <View style={styles.unfollowButton}>
-                            <Text style={styles.unfollowButtonText}>Unfollow</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                <FlatList
+                    style={styles.list}
+                    data={followers}
+                    keyExtractor={(item) => item._id}
+                    renderItem={({ item }) => (
+                        <Follower />
+                    )}
+                />
             </View>
         </SafeAreaView>
     )
@@ -60,48 +65,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
     },
 
-    profile: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
+    list: {
+        marginTop: 20,
     },
-
-    profileInfo: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        columnGap: 10,
-    },
-
-    profilePic: {
-        backgroundColor: '#000',
-        width: 55,
-        height: 55,
-        borderRadius: 100,
-    },
-
-    text: {
-        rowGap: 3,
-    },
-
-    username: {
-        fontFamily: 'Sarabun-SemiBold',
-        fontSize: 18,
-    },
-
-    name: {
-        fontFamily: 'Sarabun-Regular',
-        fontSize: 16,
-    },
-
-    unfollowButton: {
-        backgroundColor: '#EFEFEF',
-        borderRadius: 100,
-        paddingHorizontal: 20,
-        paddingVertical: 5
-    },
-
-    unfollowButtonText: {
-        fontFamily: 'Sarabun-SemiBold',
-        fontSize: 16,
-    }
 })
