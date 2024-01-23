4fb80ed2730f713dbe5109b45ecacba71a9b79cb
Ishika Jha <ishika.jha.24@dartmouth.edu>
2024-01-22 22:12:01 -0500

    fetching liked poems array working

--- literary-hub/mongo-api/index.js
+++ literary-hub/mongo-api/index.js
@@ -523,3 +523,24 @@ app.put('/mark-poem-as-read/:userId/:poemId', async (req, res) => {
     res.status(500).json({ message: 'Error marking poem as read' });
   }
 });
+
+//endpoint for getting list of liked poems
+app.get('/users/:userId/likedPoems', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const likedPoems = user.likedPoems;
+
+
+    res.json(likedPoems); 
+  } catch (error) {
+    console.error('Error fetching liked poems:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
