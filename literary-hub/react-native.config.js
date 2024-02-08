ccd4f8ab12d7ce274e9189ba1bdbd045fe5c4992
Ishika Jha <ishika.jha.24@dartmouth.edu>
2024-02-08 17:12:30 -0500

    trying firebase in app.json

--- literary-hub/react-native.config.js
+++ literary-hub/react-native.config.js
@@ -5,3 +5,22 @@ module.exports = {
     },
     assets: ["./assets/fonts/"], // stays the same
   };
+
+let plugins = [
+  [
+    'expo-facebook',
+    {
+      userTrackingPermission: false,
+    },
+  ],
+  [
+    'expo-image-picker',
+    {
+      photosPermission:
+        'The app accesses your photos to let you upload a profile picture.',
+    },
+  ],
+];
+if (process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
+  plugins.push(['sentry-expo']);
+}
